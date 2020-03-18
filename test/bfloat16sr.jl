912bb3f2a0f763ea097aaddf6a31feda022f6a54
milankl <milankloewer@gmx.de>
2020-03-18 17:28:14 +0000

    BFloat16 tests removed

--- test/bfloat16sr.jl
+++ test/bfloat16sr.jl
@@ -27,19 +27,6 @@ end
 
 @testset "Comparisons" begin
 
-    # DETERMINISTIC ROUNDING
-    @test BFloat16(1)   <  BFloat16(2)
-    @test BFloat16(1f0) <  BFloat16(2f0)
-    @test BFloat16(1.0) <  BFloat16(2.0)
-    @test BFloat16(1)   <= BFloat16(2)
-    @test BFloat16(1f0) <= BFloat16(2f0)
-    @test BFloat16(1.0) <= BFloat16(2.0)
-    @test BFloat16(2)   >  BFloat16(1)
-    @test BFloat16(2f0) >  BFloat16(1f0)
-    @test BFloat16(2)   >= BFloat16(1)
-    @test BFloat16(2f0) >= BFloat16(1f0)
-    @test BFloat16(2.0) >= BFloat16(1.0)
-
     #STOCHASTIC ROUNDING
     @test BFloat16sr(1)   <  BFloat16sr(2)
     @test BFloat16sr(1f0) <  BFloat16sr(2f0)
@@ -255,9 +242,3 @@ end
         end
     end
 end
-
-@test abs(BFloat16(-10)) == BFloat16(10)
-@test Float32(BFloat16(10)) == 1f1
-@test Float64(BFloat16(10)) == 10.0
-@test BFloat16(2) ^ BFloat16(4) == BFloat16(16)
-@test sqrt(BFloat16(4f0)) == BFloat16(2f0)
