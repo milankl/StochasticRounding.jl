de1637ee6f172bc5b7f1945e79ae0b7426bc8018
Milan K <milankloewer@gmx.de>
2020-09-03 19:00:51 +0100

    Stochastic rounding for negative numbers

--- src/bfloat16sr.jl
+++ src/bfloat16sr.jl
@@ -6,6 +6,9 @@ for f in (:sign_mask, :exponent_mask, :exponent_one,
     @eval $(f)(::Type{BFloat16sr}) = UInt16($(f)(Float32) >> 16)
 end
 
+"""Mask for both sign and exponent bits. Equiv to ~significand_mask(Float32)."""
+signexp_mask(::Type{Float32}) = 0xff80_0000
+
 iszero(x::BFloat16sr) = reinterpret(UInt16, x) & ~sign_mask(BFloat16sr) == 0x0000
 isfinite(x::BFloat16sr) = (reinterpret(UInt16,x) & exponent_mask(BFloat16sr)) != exponent_mask(BFloat16sr)
 isnan(x::BFloat16sr) = (reinterpret(UInt16,x) & ~sign_mask(BFloat16sr)) > exponent_mask(BFloat16sr)
@@ -30,8 +33,7 @@ floatmax(::BFloat16sr) = floatmax(BFloat16sr)
 # Truncation from Float32
 Base.uinttype(::Type{BFloat16sr}) = UInt16
 Base.trunc(::Type{BFloat16sr}, x::Float32) = reinterpret(BFloat16sr,
-        (reinterpret(UInt32, x) >> 16) % UInt16
-    )
+        (reinterpret(UInt32, x) >> 16) % UInt16)
 
 # same for BFloat16sr, but do not apply stochastic rounding to avoid InexactError
 round(x::BFloat16sr, r::RoundingMode{:Up}) = BFloat16sr(ceil(Float32(x)))
@@ -48,61 +50,82 @@ UInt32(x::BFloat16sr) = UInt32(Float32(x))
 UInt16(x::BFloat16sr) = UInt16(Float32(x))
 UInt8(x::BFloat16sr) = UInt8(Float32(x))
 
-"""
-    epsBF16
-Machine epsilon for BFloat16 as Float32.
-"""
 const epsBF16 = 0.0078125f0							# machine epsilon of BFloat16 as Float32
-const epsBF16_half = epsBF16/2
-const eps_quarter = 0x00004000						# a quarter of eps as Float32 sig bits
-const F32_one = reinterpret(UInt32,one(Float32))
+const epsBF16_half = epsBF16/2						# half the machine epsilon
+const eps_quarter = 0x0000_4000						# a quarter of eps as Float32 sig bits
+const F32_one = reinterpret(UInt32,one(Float32))	# Float32 one as UInt32
 
-# Conversion from Float32 with deterministic rounding - identical to BFloat16(::Float32)
+"""Convert to BFloat16sr from Float32 via round-to-nearest
+and tie to even. Identical to BFloat16(::Float32)."""
 function BFloat16sr(x::Float32)
     isnan(x) && return NaNB16sr
-	# Round to nearest even (matches TensorFlow and our convention for
-    # rounding to lower precision floating point types).
     h = reinterpret(UInt32, x)
     h += 0x7fff + ((h >> 16) & 1)
     return reinterpret(BFloat16sr, (h >> 16) % UInt16)
 end
 
-# Conversion from Float32 with distance proportional stochastic rounding
-# only used within arithmetic operations
+"""Convert to BFloat16sr from Float32 with stochastic rounding."""
 function BFloat16_stochastic_round(x::Float32)
     isnan(x) && return NaNB16sr
 
 	ui = reinterpret(UInt32, x)
 
-	# stochastic rounding
-	# e is the base 2 exponent of x (sign and signficand set to zero)
-	e = reinterpret(Float32,ui & exponent_mask(Float32))
+	# e is the base 2 exponent of x (with sign, signficand is set to zero)
+	# e.g. e is 2 for pi, e is -2 for -pi, e is 0.25 for 0.3
+	e = reinterpret(Float32,ui & signexp_mask(Float32))
 
 	# sig is the signficand (exponents & sign is masked out)
 	sig = ui & significand_mask(Float32)
 
-	# special case for rounding within 2^n <= x < 2^n+nextfloat(2^n)/4 due to doubling of eps towards nextfloat
-	q = sig < eps_quarter
+	# STOCHASTIC ROUNDING
+	# In most cases, perturb any x between x0 and x1 with a random number
+	# that is in (-ulp/2,ulp/2) where ulp is the distance between x0 and x1.
+	# ulp = e*eps, with e the next base 2 exponent to zero from x.
+
+	# However, there is a special case (aka the "quarter-case") for rounding
+	# below ulp/4 when x0 is 2^n for any n (i.e. above an exponent bit flip)
+	# due to doubling of ulp towards x1.
+	quartercase = sig < eps_quarter		# true for special case false otherwise
+
+	# frac is in most cases 0.5 to shift the randum number [0,1) to [-0.5,0.5)
+
+	# However, in the special case frac is (x-x0)/(x1-x0), that means the fraction
+	# of the distance where x is in between x0 and x1
+	# Then shift the random number [0,1) to be [frac,frac+ulp/2)
+	# such that e.g. x = x0 + ulp/8 gets perturbed to be in [x0+ulp/4,x0+5/8*ulp)
+	# and so the chance of a round-up is
+
+	adjusts the perturbation in the below-eps/4 case to be asymmetric
+	# otherwise frac is 0.5 such that the perturbation is within (-eps/2,eps/2)
 	frac = q ? reinterpret(Float32,F32_one | (sig << 7)) - 1f0 : 0.5f0
-	eps = q ? epsBF16_half : epsBF16
-	x += e*eps*(rand(Xor128[],Float32) - frac)
+	eps = q ? epsBF16_half : epsBF16	# in this case use eps/2 from previous exponent
+
+	# stochastically perturb x before rounding (equiv to stochastic rounding)
+	x += e*eps*(rand(Xor128[],Float32) - frac)					# (*)
 
     # Round to nearest after stochastic perturbation
-	ui = reinterpret(UInt32, x)
-    ui += 0x7fff + ((ui >> 16) & 1)
-    return reinterpret(BFloat16sr, (ui >> 16) % UInt16)
+    return BFloat16sr(x)
 end
 
+"""Chance that x::Float32 is round up when converted to BFloat16sr."""
 function BFloat16_chance_roundup(x::Float32)
     isnan(x) && return NaNB16sr
 	ui = reinterpret(UInt32, x)
 	# sig is the signficand (exponents & sign is masked out)
 	sig = ui & significand_mask(Float32)
+	# sig << 7, push significant bits that would be round away into the most
+	# most significant bits, then set the exponent to be equi to one(Float32)
+	# Consequently frac is fraction where x is in between x0 and x1.
+	# For x=x0, frac=0, for x halfway between x0 and x1, frac=0.5
+	# for one quarter the way, frac=1/4 etc.
+	# this equals the chance that x gets round up in stochastic rounding
+	# note that frac is in [0,1).
 	frac = reinterpret(Float32,F32_one | (sig << 7)) - 1f0
     return frac
 end
 
 # Conversions
+"""Convert BFloat16sr to Float32 by padding trailing zeros."""
 Float32(x::BFloat16sr) = reinterpret(Float32, UInt32(reinterpret(UInt16, x)) << 16)
 
 BFloat16sr(x::Float64) = BFloat16sr(Float32(x))
@@ -115,7 +138,6 @@ Float16(x::BFloat16sr) = Float16(Float32(x))
 # conversion between BFloat16 and BFloat16sr
 BFloat16(x::BFloat16sr) = reinterpret(BFloat16,x)
 BFloat16sr(x::BFloat16) = reinterpret(BFloat16sr,x)
-# Base.promote_rule(::Type{BFloat16}, ::Type{BFloat16sr}) = BFloat16
 
 # Truncation to integer types
 Base.unsafe_trunc(T::Type{<:Integer}, x::BFloat16sr) = unsafe_trunc(T, Float32(x))
@@ -125,9 +147,10 @@ for f in (:+, :-, :*, :/, :^)
 	@eval ($f)(x::BFloat16sr, y::BFloat16sr) = BFloat16_stochastic_round($(f)(Float32(x), Float32(y)))
 end
 
+# negation via signbit flip
 -(x::BFloat16sr) = reinterpret(BFloat16sr, reinterpret(UInt16, x) ⊻ sign_mask(BFloat16sr))
 
-# bit-wise & with ~sign_mask
+# absolute value by setting the signbit to zero
 abs(x::BFloat16sr) = reinterpret(BFloat16sr, reinterpret(UInt16, x) & 0x7fff)
 
 for func in (:sin,:cos,:tan,:asin,:acos,:atan,:sinh,:cosh,:tanh,:asinh,:acosh,
