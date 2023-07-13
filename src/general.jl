ff8b2fc02e9e3f34a65668bb3c1a0688c407bfea
Milan Klöwer <milankloewer@gmx.de>
2023-07-13 18:08:59 +0200

    temporary next/prevfloat for BFloat16 added

--- src/general.jl
+++ src/general.jl
@@ -1,2 +1,15 @@
 # TODO this should be implemented upstream for various number formats
 Base.ldexp(x::AbstractFloat,n::Integer) = x*2^n
+
+# TODO remove here once implemented in BFloat16s.jl
+function Base.nextfloat(x::BFloat16,n::Integer)
+    n < 0 && return prevfloat(x,-n)
+    n == 0 && return x
+    nextfloat(nextfloat(x),n-1)
+end
+
+function Base.prevfloat(x::BFloat16,n::Integer)
+    n < 0 && return nextfloat(x,-n)
+    n == 0 && return x
+    prevfloat(prevfloat(x),n-1)
+end
