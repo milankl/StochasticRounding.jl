ce95495ae371b8062ea5a4ec594584eef0b4af59
Milan <milankloewer@gmx.de>
2023-12-22 12:28:03 +0100

    promotion between stochastic and deterministic yields deterministic

--- src/promotions.jl
+++ src/promotions.jl
@@ -6,3 +6,9 @@ Base.promote_rule(::Type{Float16sr}, ::Type{BFloat16sr}) = Float32sr
 Base.promote_rule(::Type{Float16sr}, ::Type{Float64sr}) = Float64sr
 Base.promote_rule(::Type{BFloat16sr}, ::Type{Float64sr}) = Float64sr
 Base.promote_rule(::Type{Float32sr}, ::Type{Float64sr}) = Float64sr
+
+# (to be extended for new formats)
+
+# promotion between stochastic and deterministic floats yields deterministic
+Base.promote_rule(::Type{S},::Type{D}) where {S<:AbstractStochasticFloat,D<:AbstractFloat} =
+    promote_type(float(S),D)
