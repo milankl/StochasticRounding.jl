6247df2f351075059427927608906bc610e0edb9
Milan <milankloewer@gmx.de>
2023-12-22 15:24:39 +0100

    decompose added for BFloat16 (not yet in BFloat16s.jl)

--- src/bfloat16.jl
+++ src/bfloat16.jl
@@ -37,3 +37,14 @@ function Base.nextfloat(f::BFloat16, d::Integer)
     end
     reinterpret(F, fu)
 end
+
+function Base.decompose(x::BFloat16)::NTuple{3,Int}
+    isnan(x) && return 0, 0, 0
+    isinf(x) && return ifelse(x < 0, -1, 1), 0, 0
+    n = reinterpret(UInt16, x)
+    s = (n & 0x007f) % Int16
+    e = ((n & 0x7f80) >> 7) % Int
+    s |= Int16(e != 0) << 7
+    d = ifelse(signbit(x), -1, 1)
+    s, e - 134 + (e == 0), d
+end
