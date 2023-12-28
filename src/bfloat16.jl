40d8758bfebe13aa08d6b3dd289d05a4cc81c19a
Milan <milankloewer@gmx.de>
2023-12-28 20:24:33 +0100

    add round ToZero till released with BFloat16s v0.5

--- src/bfloat16.jl
+++ src/bfloat16.jl
@@ -53,3 +53,6 @@ BFloat16s.BFloat16(x::BigFloat) = BFloat16(Float32(x))
 Base.BigFloat(x::BFloat16) = BigFloat(Float32(x))
 
 Base.maxintfloat(::Type{BFloat16}) = reinterpret(BFloat16,0x4380)
+
+Base.round(x::BFloat16, r::RoundingMode{:ToZero}) = BFloat16(trunc(Float32(x)))
+Base.round(x::BFloat16, r::RoundingMode{:Down}) = BFloat16(floor(Float32(x)))
