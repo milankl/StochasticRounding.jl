59929da072e838c74a000805e8c0f504f6b06dbc
Patrick Jaap <patrick.jaap@wias-berlin.de>
2025-05-07 17:52:11 +0200

    Add test for deterministic -> stochastic

--- test/conversions.jl
+++ test/conversions.jl
@@ -1,7 +1,7 @@
-@testset "Converting Stochastic FP to BFloat16sr" begin
+@testset "Deterministic conversion of Stochastic FP to BFloat16sr" begin
     for k in 1:100000
         trueVal = randn(Float64)
-        # Convert to each deterministic type to ensure the result is 
+        # Convert to each deterministic type to ensure the result is
         # representable in all precisions
         trueVal=Float16(Float32(BFloat16(trueVal)))
         bfloatVal = BFloat16sr(trueVal)
@@ -14,10 +14,10 @@
     end
 end
 
-@testset "Converting Stochastic FP to Float16sr" begin
+@testset "Deterministic conversion of  Stochastic FP to Float16sr" begin
     for k in 1:100000
         trueVal = randn(Float64)
-        # Convert to each deterministic type to ensure the result is 
+        # Convert to each deterministic type to ensure the result is
         # representable in all precisions
         trueVal=Float16(Float32(BFloat16(trueVal)))
         bfloatVal = BFloat16sr(trueVal)
@@ -30,10 +30,10 @@ end
     end
 end
 
-@testset "Converting Stochastic FP to Float32sr" begin
+@testset "Deterministic conversion of  Stochastic FP to Float32sr" begin
     for k in 1:100000
         trueVal = randn(Float64)
-        # Convert to each deterministic type to ensure the result is 
+        # Convert to each deterministic type to ensure the result is
         # representable in all precisions
         trueVal=Float16(Float32(BFloat16(trueVal)))
         bfloatVal = BFloat16sr(trueVal)
@@ -43,5 +43,35 @@ end
         # rounding so all these 3 should be the exact same numerically
         @test float32Val == Float32sr(bfloatVal)
         @test float32Val == Float32sr(float16Val)
+    end
+end
+
+
+@testset "Stochastic conversion of Float64 to $(SR)" for SR in [Float16sr, BFloat16sr, Float32sr]
+
+    # corresponding FP type
+    FP = float(SR)
+
+    for k in 1:100
+        trueVal = randn(Float64)
+
+        # make sure that this is not representable by FP
+        if trueVal == FP(trueVal)
+            trueVal = nextfloat(trueVal)
+        end
+
+        roundedVal = SR(trueVal)
+
+        # check that rounding is not deterministic
+        is_stochastic = false
+        for l in 1:10000
+            if roundedVal != SR(trueVal)
+                is_stochastic = true
+                break
+            end
+        end
+
+        # the redundant "== true" produces a better error message in the test log
+        @test is_stochastic == true
     end
 end
